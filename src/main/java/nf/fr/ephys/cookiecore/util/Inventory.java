651d219b495e6cd58349baf6315b8017260201dd
ephys <ephyspotato@gmail.com>
2014-08-07 17:53:47 +0200

    inventory impl' part II

--- src/main/java/nf/fr/ephys/cookiecore/util/Inventory.java
+++ src/main/java/nf/fr/ephys/cookiecore/util/Inventory.java
@@ -6,9 +6,13 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import nf.fr.ephys.cookiecore.helpers.NBTHelper;
 
+/**
+ * This class is for internal use in anything that requires an inventory
+ * Use it to delegate IInventory methods
+ */
 public class Inventory implements IInventory, IWritable {
 	private int stackSize;
-	private ItemStack[] stacks;
+	public ItemStack[] stacks;
 
 	public Inventory(int nbSlots) {
 		this(nbSlots, 64);
@@ -27,22 +31,36 @@ public class Inventory implements IInventory, IWritable {
 
 	@Override
 	public ItemStack getStackInSlot(int slot) {
-		return null;
+		return stacks[slot];
 	}
 
 	@Override
 	public ItemStack decrStackSize(int slot, int amount) {
-		return null;
+		if (stacks[slot] == null) return null;
+
+		if (amount > stacks[slot].stackSize)
+			amount = stacks[slot].stackSize;
+
+		ItemStack emptied = stacks[slot].splitStack(amount);
+
+		if (stacks[slot].stackSize <= 0)
+			setInventorySlotContents(slot, null);
+
+		return emptied;
 	}
 
 	@Override
 	public ItemStack getStackInSlotOnClosing(int slot) {
-		return null;
+		ItemStack stack = stacks[slot];
+
+		setInventorySlotContents(slot, null);
+
+		return stack;
 	}
 
 	@Override
 	public void setInventorySlotContents(int slot, ItemStack stack) {
-
+		stacks[slot] = stack;
 	}
 
 	@Override
@@ -91,7 +109,7 @@ public class Inventory implements IInventory, IWritable {
 	@Override
 	public void readFromNBT(NBTTagCompound nbt) {
 		for (int i = 0; i < stacks.length; i++) {
-
+			stacks[i] = NBTHelper.getItemStack(nbt, Integer.toString(i));
 		}
 	}
 }
