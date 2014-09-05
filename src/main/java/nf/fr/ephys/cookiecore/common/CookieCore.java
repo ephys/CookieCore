876532159096862cd6107812290eb945d9fdea95
ephys <ephyspotato@gmail.com>
2014-09-05 19:00:57 +0200

    uhoh, client code being called by the server. Bad.

--- src/main/java/nf/fr/ephys/cookiecore/common/CookieCore.java
+++ src/main/java/nf/fr/ephys/cookiecore/common/CookieCore.java
@@ -8,9 +8,8 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.ModMetadata;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import net.minecraft.client.gui.GuiFlatPresets;
+import cpw.mods.fml.relauncher.Side;
 import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
@@ -19,8 +18,6 @@ import nf.fr.ephys.cookiecore.common.registryUtil.FlatPresetRegistry;
 import nf.fr.ephys.cookiecore.helpers.DebugHelper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.Arrays;
 
 @Mod(modid = CookieCore.MODID, version = CookieCore.VERSION, name = CookieCore.MODNAME)
 public class CookieCore extends DummyModContainer {
@@ -61,23 +58,22 @@ public class CookieCore extends DummyModContainer {
 
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event) {
-		if (DebugHelper.debug) {
-			// remove unwanted presets in my test worlds
+		if (event.getSide().equals(Side.CLIENT)) {
+			if (DebugHelper.debug) {
+				// remove unwanted presets in my test worlds
 
-			FlatPresetRegistry.removeByName("Classic Flat");
-			FlatPresetRegistry.removeByName("Tunnelers' Dream");
-			FlatPresetRegistry.removeByName("Water World");
-			FlatPresetRegistry.removeByName("Overworld");
-			FlatPresetRegistry.removeByName("Snowy Kingdom");
-			FlatPresetRegistry.removeByName("Bottomless Pit");
-			FlatPresetRegistry.removeByName("Desert");
+				FlatPresetRegistry.removeByName("Classic Flat");
+				FlatPresetRegistry.removeByName("Tunnelers' Dream");
+				FlatPresetRegistry.removeByName("Water World");
+				FlatPresetRegistry.removeByName("Overworld");
+				FlatPresetRegistry.removeByName("Snowy Kingdom");
+				FlatPresetRegistry.removeByName("Bottomless Pit");
+				FlatPresetRegistry.removeByName("Desert");
+			}
+
+			// add my own presets
+			FlatPresetRegistry.addPresetAt(0, FlatPresetRegistry.buildPreset("Cookie Realmn", Item.getItemFromBlock(Blocks.stained_hardened_clay), BiomeGenBase.mushroomIsland, null, new FlatLayerInfo[] {new FlatLayerInfo(63, Blocks.stained_hardened_clay), new FlatLayerInfo(1, Blocks.bedrock)}));
 		}
-
-		// add my own presets
-		FlatPresetRegistry.addPresetAt(0, FlatPresetRegistry.buildPreset("Cookie Realmn", Item.getItemFromBlock(Blocks.stained_hardened_clay), BiomeGenBase.mushroomIsland, null, new FlatLayerInfo[] {
-				new FlatLayerInfo(63, Blocks.stained_hardened_clay),
-				new FlatLayerInfo(1, Blocks.bedrock)
-		}));
 	}
 
 	public static Logger getLogger() {
