8502c8128405059271eb4d3962cc17bf88633968
Alyx <zoe@ephys.dev>
2025-09-12 18:22:04 +0200

    port to 1.20.1

--- src/main/java/be/ephys/cookiecore/core/CookieCore.java
+++ src/main/java/be/ephys/cookiecore/core/CookieCore.java
@@ -1,19 +1,19 @@
 package be.ephys.cookiecore.core;
 
-import be.ephys.cookiecore.config.Config;
 import be.ephys.cookiecore.config.ConfigSynchronizer;
-import be.ephys.cookiecore.helpers.DebugHelper;
-import be.ephys.cookiecore.registries.FlatPresetRegistry;
-import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraft.world.entity.decoration.Motive;
-import net.minecraft.world.level.biome.Biomes;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.levelgen.flat.FlatLayerInfo;
-import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.decoration.PaintingVariant;
+import net.minecraft.world.level.GameRules;
+import net.minecraft.world.level.GameType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.FlatLevelSource;
+import net.minecraft.world.level.levelgen.flat.FlatLevelGeneratorSettings;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.event.level.LevelEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -21,64 +21,60 @@ import net.minecraftforge.registries.RegistryObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.List;
+
 @Mod(CookieCore.MODID)
+@Mod.EventBusSubscriber(modid = CookieCore.MODID)
 public class CookieCore {
   public static final String MODID = "cookiecore";
 
   private static final Logger logger = LogManager.getLogger(MODID);
 
-  @Config(description = "Enable the CookieRealm Flat world preset", side = ModConfig.Type.CLIENT)
-  @Config.BooleanDefault(true)
-  public static ForgeConfigSpec.BooleanValue enableTerracottaWorldPreset;
-
-  public static Logger getLogger() {
-    return logger;
-  }
+  // TODO Move to Fundamental, as well as banner
+  public static DeferredRegister<PaintingVariant> PAINTING_TYPES = DeferredRegister.create(ForgeRegistries.PAINTING_VARIANTS, CookieCore.MODID);
+  public static RegistryObject<PaintingVariant> PAINTING_ZEN = PAINTING_TYPES.register("zen", () -> new PaintingVariant(16, 32));
 
   public CookieCore() {
     ConfigSynchronizer.synchronizeConfig();
 
     IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-    modBus.addListener(this::postInit);
-
     PAINTING_TYPES.register(modBus);
   }
 
-  // TODO Move to Fundamental, as well as banner
-  public static DeferredRegister<Motive> PAINTING_TYPES = DeferredRegister.create(ForgeRegistries.PAINTING_TYPES, CookieCore.MODID);
-  public static RegistryObject<Motive> PAINTING_ZEN = PAINTING_TYPES.register("zen",()-> new Motive(16, 32));
+  public static Logger getLogger() {
+    return logger;
+  }
 
-  public void postInit(FMLClientSetupEvent event) {
+  @SubscribeEvent
+  public static void onLevelCreateSpawnPosition(LevelEvent.CreateSpawnPosition event) {
+    ServerLevel level = (ServerLevel) event.getLevel();
 
-    if (DebugHelper.debug) {
-      // remove unwanted presets in my test worlds
-      FlatPresetRegistry.removeByName("createWorld.customize.preset.classic_flat");
-      FlatPresetRegistry.removeByName("createWorld.customize.preset.tunnelers_dream");
-      FlatPresetRegistry.removeByName("createWorld.customize.preset.water_world");
-      FlatPresetRegistry.removeByName("createWorld.customize.preset.overworld");
-      FlatPresetRegistry.removeByName("createWorld.customize.preset.snowy_kingdom");
-      FlatPresetRegistry.removeByName("createWorld.customize.preset.bottomless_pit");
-      FlatPresetRegistry.removeByName("createWorld.customize.preset.desert");
-      FlatPresetRegistry.removeByName("createWorld.customize.preset.the_void");
+    ChunkGenerator chunkGenerator = level.getChunkSource().getGenerator();
+    if (!(chunkGenerator instanceof FlatLevelSource flatLevelSource)) {
+      return;
     }
 
-    // add my own presets
-    if (enableTerracottaWorldPreset.get()) {
-      FlatPresetRegistry.addPresetAt(0,
-        FlatPresetRegistry.buildPreset(
-          new TranslatableComponent("cookiecore.superflat.cookie_realmn"),
-          Blocks.WHITE_CONCRETE,
-          Biomes.MUSHROOM_FIELDS,
-          null,
-          false,
-          false,
-          new FlatLayerInfo[]{
-            new FlatLayerInfo(63, Blocks.WHITE_CONCRETE),
-            new FlatLayerInfo(1, Blocks.BEDROCK)
-          }
-        )
-      );
+    FlatLevelGeneratorSettings settings = flatLevelSource.settings();
+    if (!settings.getBiome().is(Tags.Biomes.IS_MUSHROOM)) {
+      return;
     }
+
+    List<BlockState> layers = settings.getLayers();
+    if (layers.size() < 64) {
+      return;
+    }
+
+    level.getServer().setDefaultGameType(GameType.CREATIVE);
+
+    GameRules gameRules = level.getGameRules();
+    gameRules.getRule(GameRules.RULE_DAYLIGHT).set(false, level.getServer());
+    gameRules.getRule(GameRules.RULE_WEATHER_CYCLE).set(false, level.getServer());
+    gameRules.getRule(GameRules.RULE_DOINSOMNIA).set(false, level.getServer());
+    gameRules.getRule(GameRules.RULE_DO_IMMEDIATE_RESPAWN).set(true, level.getServer());
+    gameRules.getRule(GameRules.RULE_DO_PATROL_SPAWNING).set(false, level.getServer());
+
+    // Set the time to midday
+    level.setDayTime(6000);
   }
 }
